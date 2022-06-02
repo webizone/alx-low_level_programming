5e6313c753a0198bc838a0300b182db135c4382c
webizone <webizone30@gmail.com>
2022-06-02 15:40:18 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * return ; 0 (as a result)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
