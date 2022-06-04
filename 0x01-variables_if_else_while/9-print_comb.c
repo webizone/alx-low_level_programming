d223c23d19bd73a2a8a4178fdce40fd0ca934453
webizone <webizone30@gmail.com>
2022-06-03 17:48:00 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ **/
+int main(void)
+{
+	int a;
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
