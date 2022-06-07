f561928a2599120dd2e8806f1cc3f0e551db3e12
webizone <webizone30@gmail.com>
2022-06-07 16:51:22 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *
+ *  
+ *
+ *   *print_alphabet_x10 - display alphabet 10 times
+ *
+ *    *Return: 0
+ *
+ *     */
+
+void print_alphabet_x10(void)
+
+{
+
+	char c;
+
+	char i;
+
+
+
+	for (i = 0; i < 10; i++)
+
+	{
+
+	
+                                           
+	  for (c = 'a'; c <= 'z'; c++)
+
+			{
+
+				_putchar(c);
+
+			}
+
+		_putchar('\n');
+
+	}
+
+}
