cf108e1d27abd53e6dbc146687ec142b4f52b347
webizone <webizone30@gmail.com>
2022-06-07 18:00:46 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: display last digit
+ * Return: last_num;
+ */
+int print_last_digit(int n)
+
+{
+
+		int last_num = 0;
+
+
+
+		
+
+						
+
+                      				last_num = n % 10;
+
+					if (n < 0)
+
+								last_num = last_num * -1;
+
+						_putchar(last_num + '0');
+
+							return (last_num);
+
+}
