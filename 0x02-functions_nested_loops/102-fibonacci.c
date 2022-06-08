56963cb076278d77fb5e38f9dc6a1b4f21d88b04
webizone <webizone30@gmail.com>
2022-06-07 21:39:53 -0700

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main -main function
+ * @void: parameters
+ * Return: 0
+ */
+int main(void)
+{
+	long int a, b, x;
+	int i;
+	a = 1;
+	b = 2;
+	printf("%ld, ", a);
+
+	printf("%ld, ", b);
+
+
+
+	for (i = 3; i <= 50; i++)
+
+	{
+
+		x = a + b;
+
+		a = b;
+
+		b = x;
+
+		if (i != 50)
+			printf("%li, ", x);
+
+		else
+		 printf("%li", x);
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
