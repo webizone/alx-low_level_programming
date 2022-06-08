ce90ec120c63d3e268ae9d46c34ee9b081443b33
webizone <webizone30@gmail.com>
2022-06-07 18:44:40 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - print times_table
+ */
+void times_table(void)
+{
+	int i, j;
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	if (i * j < 10)
+	{
+	_putchar(i * j + '0');
+	if (j != 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	if (i * (j + 1) < 10)
+	{
+	if (j != 9)
+	_putchar(' ');
+	{
+	{
+	else
+	{
+	_putchar((i * j) / 10 + '0');
+        _putchar((i * j) % 10 + '0');
+        if (j != 9)
+	{
+	_putchar(',');
+        _putchar(' ');
+        }
+        }
+        }
+        _putchar('\n');
+        }
+        }	
+		
