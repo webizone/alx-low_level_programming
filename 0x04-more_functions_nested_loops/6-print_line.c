51195eefcecfc4a526b0f095a84ef525980383ea
webizone <webizone30@gmail.com>
2022-06-09 14:49:44 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *
+ * print_line - check for the value of n
+ *
+ * @n: integer
+ *
+ * Return: no return
+ *
+ */
+void print_line(int n)
+
+{
+
+	int a = '_';
+
+	int b;
+
+
+
+	for (b = 0; b < n; b++)
+
+	{
+
+		if (n <= 0)
+
+			_putchar('\n');
+
+		else
+
+			_putchar(a);
+
+	}
+
+	_putchar('\n');
+
+}
