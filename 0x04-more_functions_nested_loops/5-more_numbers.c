eb037f677e65b16b35995e48ae10ca21cd825dad
webizone <webizone30@gmail.com>
2022-06-09 14:46:19 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *
+ * more_numbers - print numbers
+ *
+ * Return: nothing
+ *
+ */
+void more_numbers(void)
+
+{
+
+	int a, b;
+	for (a = 1; a <= 10; a++)
+
+	{
+
+		for (b = 0; b <= 14; b++)
+
+		{
+
+			if (b >= 10)
+
+				_putchar(b / 10 + '0');
+
+			_putchar(b % 10 + '0');
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}
