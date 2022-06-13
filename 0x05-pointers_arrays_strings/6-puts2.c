99ef8cec831ee945bd4d2036e54d179c7f14893a
webizone <webizone30@gmail.com>
2022-06-13 14:49:59 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *puts2 - print every other charachter of a string
+ *
+ *@str: string
+ *
+ *Return: nothing
+ */
+void puts2(char *str)
+
+{
+
+	int i;
+
+	i = 0;
+
+
+
+	while (str[i])
+
+	{
+
+		if (i % 2 == 0)
+
+			_putchar(str[i]);
+
+		i++;
+
+	}
+
+	_putchar('\n');
+
+}
