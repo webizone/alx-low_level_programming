f4b280966e2553fcd72aed702139b9795e3eb7d2
webizone <webizone30@gmail.com>
2022-06-24 00:28:05 -0700

    5 update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,3 +16,22 @@ int sqrt_check(int g, int c)
 		return (-1);
 	return (sqrt_check(g + 1, c));
 }
+
+int _sqrt_recursion(int n)
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *
+ *@n: integer to find sqrt o
+ *
+ *Return: natural square root or -1
+ */
+{
+
+		if (n == 0)
+
+					return (0);
+
+			return (check(-1, n));
+
+}
